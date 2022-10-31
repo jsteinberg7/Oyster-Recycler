6ca22108e33a912622c4d410b84884b89680fad3
nicholas casey <ncasey@terpmail.umd.edu>
2022-10-31 19:20:28 -0400

    login pages

---
+++ Restaurant App/app/src/main/java/com/example/firebaseemailpasswordexample/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.firebaseemailpasswordexample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.firebaseemailpasswordexample.databinding.MainActivityBinding
+import com.google.firebase.FirebaseApp
+import com.google.firebase.provider.FirebaseInitProvider
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // Initialize Firebase APIs
+        FirebaseInitProvider()
+
+        // Use the provided ViewBinding class to inflate the
+        // layout and set content view to the root view.
+        setContentView(MainActivityBinding.inflate(layoutInflater).root)
+    }
+}
