a5476b053d156524932e407162a6f56d5f180f39
ncasey <ncasey@terpmail.umd.edu>
2022-11-27 14:01:19 -0500

    Driver app start

--- app/src/main/java/com/example/firebaseemailpasswordexample/DashboardFragment.kt
+++ app/src/main/java/com/example/firebaseemailpasswordexample/DashboardFragment.kt
@@ -7,10 +7,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.firebaseemailpasswordexample.databinding.DashboardFragmentBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class DashboardFragment : Fragment() {
+
+    // TODO: Query FireStore for nearest 10 locations
+    var itemsList = listOf("name 1 \t\t 12 miles", "name 2 \t\t 17 miles", "name 3 \t\t 27 miles", "name 4 \t\t 37 miles")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,6 +24,8 @@ class DashboardFragment : Fragment() {
         // Use the provided ViewBinding class to inflate
         // the layout and then return the root view.
         val binding = DashboardFragmentBinding.inflate(inflater, container, false)
+        binding.list.layoutManager = LinearLayoutManager(context)
+        binding.list.adapter = TitlesRecyclerViewAdapter(itemsList, this)
 
         binding.logout.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
@@ -35,5 +41,12 @@ class DashboardFragment : Fragment() {
 
         // Return the root view.
         return binding.root
+    }
+
+    fun onItemClick(index: Int): Boolean {
+        Toast.makeText(requireContext(), itemsList[index], Toast.LENGTH_SHORT)
+
+        // Always allow items to be selected in this app.
+        return true
     }
 }
---
+++ app/src/main/java/com/example/firebaseemailpasswordexample/DriverRecyclerViewAdapter.kt
@@ -0,0 +1,93 @@
+package com.example.firebaseemailpasswordexample
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.firebaseemailpasswordexample.databinding.TitleLayoutBinding
+
+internal class TitlesRecyclerViewAdapter(
+    private val values: List<String>,
+    private val owner: DashboardFragment
+) : RecyclerView.Adapter<TitlesRecyclerViewAdapter.ViewHolder>() {
+
+    /** Keeps track of single selection state. */
+    private var lastCheckedPosition = -1
+
+    /**
+     * Called [RecyclerView] to create a new [ViewHolder]
+     * that contains an list item [View]. [onBindViewHolder]
+     * will be called next.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            // Use the provided view binding class to inflate the item layout.
+            TitleLayoutBinding.inflate(
+                LayoutInflater.from(parent.context), parent, false
+            )
+        )
+    }
+
+    /**
+     * Called by [RecyclerView] to bind item at [position] to the passed
+     * [ViewHolder]. The [RecyclerView] maintains only a small number
+     * of [ViewHolder] instances and therefore needs to recycle, or
+     * rebind, those instances as the user scrolls new items into view.
+     */
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = values[position]
+    }
+
+    /**
+     * Called by [RecyclerView] when iterating through the list items.
+     */
+    override fun getItemCount(): Int {
+        return values.size
+    }
+
+    /**
+     * Sets the current selection (checked state) to the specified item.
+     */
+    fun select(index: Int) {
+        if (lastCheckedPosition != index) {
+            if (lastCheckedPosition != -1) {
+                notifyItemChanged(lastCheckedPosition)
+            }
+            lastCheckedPosition = index
+            notifyItemChanged(index)
+        }
+    }
+
+    /**
+     * A custom [ViewHolder] used to display each list item
+     * and for handling button click input events.
+     */
+    inner class ViewHolder(binding: TitleLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        var textView = binding.textView
+
+        init {
+            // Set a button click listener to notify the
+            // owner when an item is clicked.
+            textView.setOnClickListener {
+                // Query the adapter position of this item.
+                val position = values.indexOf(textView.text)
+
+                // Only process click if this item is not already checked.
+                if (position != lastCheckedPosition) {
+                    // Clear the check state of the previously checked item.
+                    if (lastCheckedPosition >= 0 && position != lastCheckedPosition) {
+                        notifyItemChanged(lastCheckedPosition)
+                    }
+
+                    // Show the item in a checked state.
+                    lastCheckedPosition = position
+
+                    // Notify owner that this item was clicked.
+                    owner.onItemClick(position)
+                }
+            }
+        }
+    }
+}
