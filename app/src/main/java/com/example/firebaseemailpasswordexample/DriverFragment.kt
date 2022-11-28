3bbcfbde77ced5a00b8e6dd313ac89bfeb1af14e
nicholas casey <ncasey@terpmail.umd.edu>
2022-11-27 20:05:16 -0500

    mostly finished

--- app/src/main/java/com/example/firebaseemailpasswordexample/DriverFragment.kt
+++ app/src/main/java/com/example/firebaseemailpasswordexample/DriverFragment.kt
@@ -1,6 +1,9 @@
 package com.example.firebaseemailpasswordexample
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,16 +11,16 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.firebaseemailpasswordexample.databinding.TitlesListFragmentBinding
+import com.example.firebaseemailpasswordexample.databinding.DriverFragmentBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class DriverFragment : Fragment() {
 
-    // TODO: Query FireStore for nearest 10-15 locations
-    var itemsList = listOf("name 1 \t-\t 12 miles away", "name 2 \t-\t 17 miles away", "name 3 \t-\t 27 miles away", "name 4 \t-\t 37 miles away")
+    // TODO: Query FireStore for driver's pickups and their adresses
+    var itemsList = listOf("name 1", "name 2", "name 3", "name 4")
     var addressList = listOf("18311 Leedstown Way", "12412 Hooper Court", "535 Lakeshore Drive", "260 Allumnai Mall")
-    var ind = -1
-    private lateinit var binding: TitlesListFragmentBinding
+    var idx = -1
+    private lateinit var binding: DriverFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -26,7 +29,7 @@ class DriverFragment : Fragment() {
     ): View {
         // Use the provided ViewBinding class to inflate
         // the layout and then return the root view.
-        binding = TitlesListFragmentBinding.inflate(inflater, container, false)
+        binding = DriverFragmentBinding.inflate(inflater, container, false)
         binding.list.layoutManager = LinearLayoutManager(context)
         binding.list.adapter = DriverRecyclerViewAdapter(itemsList, this)
 
@@ -42,26 +45,51 @@ class DriverFragment : Fragment() {
             findNavController().popBackStack(R.id.mainFragment, false)
         }
 
-        binding.refresh.setOnClickListener {
-            // TODO: Query FireStore for nearest 10-15 locations
-            itemsList = listOf("name 5 \t-\t 12 miles away", "name 6 \t-\t 17 miles away", "name 7 \t-\t 27 miles away", "name 8 \t-\t 37 miles away")
+        binding.cancel.setOnClickListener {
+            // TODO: Query FireStore to take job off driver and then query to update lists
+            itemsList = itemsList.filter{itemsList.indexOf(it) != idx}
             binding.list.adapter = DriverRecyclerViewAdapter(itemsList, this)
         }
 
-        binding.accept.setOnClickListener {
-            if (ind >= 0) {
-                // TODO: query to add this pickup to this driver
-                Toast.makeText(requireContext(), itemsList[ind] + " added", Toast.LENGTH_SHORT).show()
+        binding.finish.setOnClickListener {
+            if (idx >= 0) {
+                // TODO: query to get rid of job on restaurant and driver and query to update list
+                Toast.makeText(requireContext(), itemsList[idx] + " finished", Toast.LENGTH_SHORT).show()
+                itemsList = itemsList.filter{itemsList.indexOf(it) != idx}
+                binding.list.adapter = DriverRecyclerViewAdapter(itemsList, this)
             }
         }
 
+        binding.directions.setOnClickListener {
+            try {
+                Log.i("test", "trying")
+                // Process text for network transmission
+                val address = addressList[idx].replace(' ', '+')
+
+                val geoIntent = Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse("geo:0,0?q=$address")
+                )
+                activity?.let { it1 ->
+                    geoIntent.resolveActivity(it1.packageManager)?.let {
+                        startActivity(geoIntent)
+                    }
+                }
+
+            } catch (e: Exception) {
+                // Log any error messages to LogCat using Log.e()
+                Log.e("Error", e.toString())
+            }
+        }
         // Return the root view.
         return binding.root
     }
 
     fun onItemClick(index: Int): Boolean {
-        ind = index
-        binding.accept.visibility = 1
+        idx = index
+        binding.finish.visibility = 1
+        binding.cancel.visibility = 1
+        binding.directions.visibility = 1
         (binding.list.adapter as DriverRecyclerViewAdapter).select(index)
         // Always allow items to be selected in this app.
         return true
